66329fc881d6d26702184fc981a5090449ddf60b
jordan.woodruff <jordan.woodruff@tylertech.com>
2023-02-01 19:31:31 -0600

    changed my mind about this design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {PeerageGeneratorComponent} from "./components/peerage-generator/peerage-generator.component";
 
-const routes: Routes = [ {path: "**", component: PeerageGeneratorComponent}];
+
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
