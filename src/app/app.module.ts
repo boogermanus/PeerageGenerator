f997bfc3844393490795f4f8f8405c844551622b
jordan.woodruff <jordan.woodruff@tylertech.com>
2023-02-01 19:33:23 -0600

    I could have just reverted this

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PeerageGeneratorComponent } from './components/peerage-generator/peerage-generator.component';
 @NgModule({
   declarations: [
     AppComponent,
+    PeerageGeneratorComponent,
   ],
   imports: [
     BrowserModule,
