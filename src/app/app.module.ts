f514c0a8827506de53a6e2553053e88bad307e12
jordan.woodruff <jordan.woodruff@tylertech.com>
2023-02-01 20:23:20 -0600

    add gender

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { PeerageGeneratorComponent } from './components/peerage-generator/peerage-generator.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {PeerageGeneratorComponent} from './components/peerage-generator/peerage-generator.component';
 import {MatCardModule} from "@angular/material/card";
-import { OptionsComponent } from './components/options/options.component';
+import {OptionsComponent} from './components/options/options.component';
+import {MatSelectModule} from "@angular/material/select";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatOptionModule} from "@angular/material/core";
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,8 +22,11 @@ import { OptionsComponent } from './components/options/options.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatCardModule,
+    MatFormFieldModule,
+    MatSelectModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
